435eb33c4de3e5dc557f9ec118904959f23c0f66
pausia <kenojiaa@gmail.com>
2023-09-17 07:57:46 +0700

    saya menambah sedikit perubahan dibagian chartui.blade lagi

--- public/question/assets/js/chart.js
+++ public/question/assets/js/chart.js
@@ -3,76 +3,66 @@ const chartData = {
   data: [30, 17, 10, 7, 30],
 };
 
-const myChart = document.querySelector(".my-chart");
-const ul = document.querySelector(".programming-stats .details ul");
+const tataPamongChart = document.getElementById("tata-pamong-chart");
+const penelitianChart = document.getElementById("penelitian-chart");
 
-new Chart(myChart, {
+const tataPamongDetails = document.getElementById("tata-pamong-details");
+const penelitianDetails = document.getElementById("penelitian-details");
+
+new Chart(tataPamongChart, {
   type: "doughnut",
   data: {
-    labels: chartData.labels,
-    datasets: [
-      {
-        label: "Tingkat Kepuasan",
-        data: chartData.data,
-      },
-    ],
+      labels: chartData.labels,
+      datasets: [
+          {
+              label: "Tingkat Kepuasan",
+              data: chartData.data,
+          },
+      ],
   },
   options: {
-    borderWidth: 10,
-    borderRadius: 2,
-    hoverBorderWidth: 0,
-    plugins: {
-      legend: {
-        display: false,
+      borderWidth: 10,
+      borderRadius: 2,
+      hoverBorderWidth: 0,
+      plugins: {
+          legend: {
+              display: false,
+          },
       },
-    },
   },
 });
 
-const populateUl = () => {
-  chartData.labels.forEach((l, i) => {
-    let li = document.createElement("li");
-    li.innerHTML = `${l}: <span class='percentage'>${chartData.data[i]}%</span>`;
-    ul.appendChild(li);
-  });
-};
-
-populateUl();
-
-
-
-const myChart1 = document.querySelector(".my-chart1");
-const ul2 = document.querySelector(".programming-stats .details .ul2");
-
-new Chart(myChart1, {
+new Chart(penelitianChart, {
   type: "doughnut",
   data: {
-    labels: chartData.labels,
-    datasets: [
-      {
-        label: "Tingkat Kepuasan",
-        data: chartData.data,
-      },
-    ],
+      labels: chartData.labels,
+      datasets: [
+          {
+              label: "Tingkat Kepuasan",
+              data: chartData.data,
+          },
+      ],
   },
   options: {
-    borderWidth: 10,
-    borderRadius: 2,
-    hoverBorderWidth: 0,
-    plugins: {
-      legend: {
-        display: false,
+      borderWidth: 10,
+      borderRadius: 2,
+      hoverBorderWidth: 0,
+      plugins: {
+          legend: {
+              display: false,
+          },
       },
-    },
   },
 });
 
-const populateUl1 = () => {
+const populateUl = (ul, chartData) => {
+  ul.innerHTML = "";
   chartData.labels.forEach((l, i) => {
-    let li = document.createElement("li");
-    li.innerHTML = `${l}: <span class='percentage'>${chartData.data[i]}%</span>`;
-    ul2.appendChild(li);
+      let li = document.createElement("li");
+      li.innerHTML = `${l}: <span class='percentage'>${chartData.data[i]}%</span>`;
+      ul.appendChild(li);
   });
 };
 
-populateUl1();
+populateUl(tataPamongDetails, chartData);
+populateUl(penelitianDetails, chartData);
